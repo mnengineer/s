031c3493195728e7ec5cbf1283b3860704d9f6d9
mnengineer <masashiii0115@gmail.com>
2023-08-12 19:30:37 +0900

    feat: TextFormにTextWidgetFiled追加

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -14,9 +14,23 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(
         title: const Text('Add a New Item'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(12),
-        child: Text('The form'),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Item Name'),
+                ),
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
