57708c4b9b33fef19a6c4b26e0f169ccee3891e9
mnengineer <masashiii0115@gmail.com>
2023-08-14 06:37:36 +0900

    feat: FirebaseにPOSTリクエスト

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
@@ -21,6 +23,23 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      final url = Uri.https(
+        'flutter-prep-d8e11-default-rtdb.firebaseio.com',
+        'shopping-list.json',
+      );
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
       Navigator.of(context).pop(GroceryItem(
           id: DateTime.now().toString(),
           name: _enteredName,
