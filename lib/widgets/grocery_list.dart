ec76f8135e856570fa14b28e1579d957041e41b0
mnengineer <masashiii0115@gmail.com>
2023-08-14 07:42:26 +0900

    feat: アイテム削除時のDELETEリクエスト送信

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -28,7 +28,7 @@ class _GroceryListState extends State<GroceryList> {
 
   void _loadItems() async {
     final url = Uri.https(
-      'flfdutter-prep-d8e11-default-rtdb.firebaseio.com',
+      'flutter-prep-d8e11-default-rtdb.firebaseio.com',
       'shopping-list.json',
     );
     final response = await http.get(url);
@@ -75,10 +75,21 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    final url = Uri.https(
+      'flutter-prep-d8e11-default-rtdb.firebaseio.com',
+      'shopping-list/${item.id}.json',
+    );
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
